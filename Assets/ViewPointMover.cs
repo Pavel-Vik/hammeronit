2394226f9ea95ddffd89b65dfbb7f10fdbabb7c2
Pavel-Vik <52125928+Pavel-Vik@users.noreply.github.com>
2020-06-13 13:37:30 +0800

    Plank and ViewPointMover

---
+++ Assets/ViewPointMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewPointMover : MonoBehaviour
+{
+    public float moveSpeed = 2;
+
+    private bool isFinish = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFinish == false)
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+            isFinish = true;
+    }
+}
