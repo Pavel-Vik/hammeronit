33dd689a5dccc0c7ebcfae83f023edc884dbe044
Pavel-Vik <52125928+Pavel-Vik@users.noreply.github.com>
2020-06-13 17:12:05 +0800

    My today final commit

---
+++ Assets/Scripts/BigNail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigNail : MonoBehaviour
+{
+    //public Transform nailRenderer;
+    public float downDepth = 0;
+    //public int hitsCount = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hammer")
+        {
+                gameObject.transform.position = new Vector3(0f, transform.position.y - downDepth, transform.position.z);
+        }
+    }
+}
