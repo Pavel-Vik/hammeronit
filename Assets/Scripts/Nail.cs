c538f5a0558c0a63d0b9b2244b969ccb7c48516e
Pavel-Vik <52125928+Pavel-Vik@users.noreply.github.com>
2020-06-13 16:14:55 +0800

    Nails

---
+++ Assets/Scripts/Nail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nail : MonoBehaviour
+{
+    public Transform nailRenderer;
+    public float downDepth = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hammer")
+        {
+            nailRenderer.transform.position = new Vector3(0f, downDepth, transform.position.z);
+            Debug.Log("POPAL");
+        }
+    }
+}
