680931b382d4eea436fd2b3555ab61f27b17548e
Pavel-Vik <52125928+Pavel-Vik@users.noreply.github.com>
2020-06-14 14:25:40 +0800

    Cracks

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Hammer : MonoBehaviour
 {
     private Animator anim;
+    public GameObject crack;
+
+    private bool isNail = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,22 @@ public class Hammer : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
             anim.SetTrigger("Attack");
+
+        Debug.Log("Is nail" + isNail);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.tag == "Nail" || other.tag == "Finish")
+            isNail = true;
+
+        if (other.tag == "Plank" && isNail == false)
+            Instantiate(crack, new Vector3(0f, 0.14f, transform.position.z), Quaternion.Euler(90, 0, 0));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isNail = false;
     }
 }
